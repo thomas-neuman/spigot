e9f66718f4172a4913d789d5c24f3fa27333c0ec
Thomas Neuman <neuman.tn@gmail.com>
2020-03-07 18:04:27 +0000

    Flesh out NknRouter implementation, with locking

--- nkn/client.go
+++ nkn/client.go
@@ -3,7 +3,6 @@ package nkn
 import (
 	"encoding/hex"
 	"log"
-	"net"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -48,10 +47,6 @@ func NewNknClient(privateSeed string, localAddr string) (*NknClient, error) {
 	return c, nil
 }
 
-func (c *NknClient) getNknAddress(ip4 net.IPAddr) ([]string, error) {
-	return []string{""}, nil
-}
-
 // Implement packets.PacketProcessor
 func (c *NknClient) Process(input gopacket.Packet) (output gopacket.Packet, consumed bool) {
 	output = input
@@ -61,13 +56,17 @@ func (c *NknClient) Process(input gopacket.Packet) (output gopacket.Packet, cons
 	if ip4Layer != nil {
 		log.Println("IPv4 message!")
 
-		/*
-		dests, err := c.getNknAddress()
+		ip4 := ip4Layer.(*layers.IPv4)
+
+		dests, err := c.router.RouteTo(ip4.DstIP.String())
 		if err != nil {
-			log.Println("Could not get destination(s) for NKN message!")
+			log.Println("Could not get destination(s) for NKN message!", err)
 			return
 		}
 
+		log.Println("Got destination(s):", dests)
+
+		/*
 		err = c.client.Send(dests, input.Data())
 		if err != nil {
 			log.Println("Failed to send NKN message!")
--- nkn/router.go
+++ nkn/router.go
@@ -3,16 +3,20 @@ package nkn
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 
 type NknRouter struct {
-	ipToNknMap map[string]string
-	nknToIpMap map[string]string
+	lock		sync.Locker
+	ipToNknMap	map[string]string
+	nknToIpMap	map[string]string
 }
 
 func NewNknRouter() *NknRouter {
-	r := &NknRouter{}
+	r := &NknRouter{
+		lock: &sync.Mutex{},
+	}
 	r.ipToNknMap = make(map[string]string)
 	r.nknToIpMap = make(map[string]string)
 
@@ -20,6 +24,9 @@ func NewNknRouter() *NknRouter {
 }
 
 func (r *NknRouter) AddRoute(ipAddr string, nknAddr string) error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
 	if net.ParseIP(ipAddr) == nil {
 		return fmt.Errorf("Cannot add route for invalid IP address: %v", ipAddr)
 	}
@@ -28,4 +35,39 @@ func (r *NknRouter) AddRoute(ipAddr string, nknAddr string) error {
 	r.nknToIpMap[nknAddr] = ipAddr
 
 	return nil
+}
+
+func (r *NknRouter) RemoveRoute(ipAddr string, nknAddr string) error {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	if net.ParseIP(ipAddr) == nil {
+		return fmt.Errorf("Cannot add route for invalid IP address: %v", ipAddr)
+	}
+
+	actual, ok := r.ipToNknMap[ipAddr]
+	if !ok || actual != nknAddr {
+		return nil
+	}
+
+	delete(r.ipToNknMap, ipAddr)
+	delete(r.nknToIpMap, nknAddr)
+
+	return nil
+}
+
+func (r *NknRouter) RouteTo(ipAddr string) (nknAddrs []string, err error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	if net.ParseIP(ipAddr) == nil {
+		return []string{}, fmt.Errorf("Cannot get route for invalid IP address: %v", ipAddr)
+	}
+
+	dest, ok := r.ipToNknMap[ipAddr]
+	if !ok {
+		return []string{}, fmt.Errorf("No route known for %v", ipAddr)
+	}
+
+	return []string{dest}, nil
 }
